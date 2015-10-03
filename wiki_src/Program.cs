b2a1a6f9f2e62199f9643af50eaa7c6c33382a67
annafatkina <annacplus@yandex.ru>
2015-10-04 00:15:25 +0300

    - Sources for wiki added

---
+++ wiki_src/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleex
+{
+    class Program
+    {
+        [DllImport("SimpleDLL.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "sum")]
+        public static extern int sum(int a, int b);
+
+        static void Main()
+        {
+            int a = 5;
+            int b = 10;
+            var c = sum(a, b);
+        }
+    }
+}
---
+++ wiki_src/Simple.cpp
@@ -0,0 +1,8 @@
+extern "C"
+{
+	__declspec(dllexport) int sum(int a, int b);
+}
+int sum(int a, int b)
+{
+	return a + b;
+}
