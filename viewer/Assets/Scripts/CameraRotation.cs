fcff08eacee26352d1dc249b57bc05fdaad76bca
Nofuq <goshus23041995@gmail.com>
2015-10-07 23:20:51 +0300

    - more utility
    no more free rotation
    -left click is to rotate around
    zooming remade
    now can focus camera on details(by right click)

---
+++ viewer/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour {
+    public float rotatesens = 2.0f;
+    float Range = 60.0f;
+    
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Camera.main.fieldOfView>3.0f && Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            Camera.main.fieldOfView -= 1.0f;
+        }
+        if (Camera.main.fieldOfView < 50.0f && Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            Camera.main.fieldOfView += 1.0f;
+        }
+        float rotx = Input.GetAxis("Mouse X") * rotatesens;
+        float roty  = -Input.GetAxis("Mouse Y") * rotatesens;
+        if (Input.GetKey("mouse 1")) Camera.main.transform.Rotate(roty, rotx, 0);
+    }
+}
---
+++ viewer/Assets/Scripts/move.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+    public float rotatesens = 2.0f;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float rot = Input.GetAxis("Mouse X") * rotatesens;
+        float upSpeed = Input.GetAxis("Vertical");
+        float sideSpeed = Input.GetAxis("Horizontal");
+        
+
+
+        transform.Rotate(upSpeed, sideSpeed, 0);
+        if(Input.GetKey("mouse 0")) transform.Rotate(0, 0, rot);
+
+    }
+}
